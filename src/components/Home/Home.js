2bfbc31c4076cb3d4cadcc5834abf9b733088e6c
Oleh.Scherbak <oleh.scherbak@ukr.net>
2023-03-16 22:01:45 +0200

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <div className={css.container}>
       <p className={css.greetings}>
-        Hi,
-        <br />
-        <br /> log in to access your contacts <br />
+        log in to access your contacts <br />
         <br />
         or register to create your own phonebook
       </p>
